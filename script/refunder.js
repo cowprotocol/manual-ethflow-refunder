6373371a27afd06b1a7bcf5b7e0cca0d6257cbf5
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-01-22 11:52:20 +0100

    Await the new tx

--- script/refunder.js
+++ script/refunder.js
@@ -34,7 +34,7 @@ async function main() {
   const new_tx = await signer.sendTransaction(new_raw_tx);
   console.log("Mining transaction...");
   // Waiting for the transaction to be mined
-  const new_receipt = await tx.wait();
+  const new_receipt = await new_tx.wait();
   // The transaction is now on chain!
   console.log(`Mined transaction https://${network}.etherscan.io/tx/${new_receipt.transactionHash}`);
 }
