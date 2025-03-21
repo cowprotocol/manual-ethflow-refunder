e484e2677fafa876f51e8611dafb0608fd9986e8
ilya <ilya@cow.fi>
2025-03-21 14:04:36 +0000

    Fix the gas estimation

--- script/refunder.js
+++ script/refunder.js
@@ -1,13 +1,6 @@
 const ethers = require("ethers");
-const readline = require("readline");
-const yn = require("yn").default;
 const ABI = require("../abi/ethflow.json");
 require("dotenv").config();
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.NODE_URL);
@@ -19,16 +12,23 @@ async function main() {
   const iface = new ethers.utils.Interface(ABI);
   const order_placement_event_hash =
     "0xcf5f9de2984132265203b5c335b25727702ca77262ff622e136baa7362bf1da9";
-  const log = receipt.logs.find(
+  const logs = receipt.logs.filter(
     (log) =>
       // topic0 is required by the iface.parseLog() function
       log.topics[0] === order_placement_event_hash
   );
-  if (!log) {
+
+  if (logs.length === 0) {
     throw new Error(
       `No matching log found with the order placement event hash: ${order_placement_event_hash}`
     );
+  } else if (logs.length > 1) {
+    throw new Error(
+      `More than one log found with the order placement event hash: ${order_placement_event_hash}`
+    );
   }
+
+  const log = logs[0];
   const ethflow_address = ethers.utils.getAddress(log.address);
   const order = iface.parseLog(log).args.order;
   console.log(
@@ -36,40 +36,17 @@ async function main() {
     order
   );
 
-  if (receipt.to.toLowerCase() !== ethflow_address.toLowerCase()) {
-    console.log(
-      "⚠️ Warning: This transaction was not a direct interaction with the eth-flow contract!"
-    );
-
-    const user_response = await askForConfirmation(
-      "Do you want to proceed with the refund? (yes/no): "
-    );
-    if (!user_response) {
-      console.log("Refund process aborted by user.");
-      return;
-    }
-  }
-
-  const ethflow_order = ethers.utils.defaultAbiCoder.decode(
-    [iface.getFunction("invalidateOrder").inputs[0]],
-    log.data
-  );
-  const invalidate_order_data = iface.encodeFunctionData("invalidateOrder", [
-    ethflow_order.order,
-  ]);
-
-  // Creating and sending the transaction object
-  const new_raw_tx = {
-    to: ethflow_address,
-    data: invalidate_order_data,
-    value: "0x0",
-  };
-  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const signed_tx = await signer.signTransaction(new_raw_tx);
+  const { gas_estimate_tx, raw_tx } = await buildInvalidateOrderTx({
+    ethflow_address,
+    tx,
+    log,
+    provider,
+    iface,
+  });
   // checks whether the gas is failing
-  const gas_estimation = await provider.estimateGas(signed_tx);
+  const gas_estimation = await provider.estimateGas(gas_estimate_tx);
   // Creating a signing account from a private key
-  const new_tx = await signer.sendTransaction(new_raw_tx);
+  const new_tx = await signer.sendTransaction(raw_tx);
   console.log("Mining transaction...");
   // Waiting for the transaction to be mined
   const new_receipt = await new_tx.wait();
@@ -79,12 +56,52 @@ async function main() {
   );
 }
 
-async function askForConfirmation(question) {
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      resolve(yn(answer, { default: false }));
-    });
-  });
+async function buildInvalidateOrderTx({
+  tx,
+  ethflow_address,
+  log,
+  provider,
+  iface,
+}) {
+  if (tx.to.toLowerCase() !== ethflow_address.toLowerCase()) {
+    console.log(
+      "Detected a tx that wasn't interacted with the ETHflow contract directly"
+    );
+
+    const [order] = ethers.utils.defaultAbiCoder.decode(
+      [iface.getFunction("invalidateOrder").inputs[0]],
+      log.data
+    );
+
+    const invalidate_order_data = iface.encodeFunctionData("invalidateOrder", [
+      order,
+    ]);
+
+    const new_raw_tx = {
+      to: ethflow_address,
+      data: invalidate_order_data,
+      value: "0x0",
+    };
+
+    if (!process.env.PRIVATE_KEY) {
+      throw new Error(
+        "PRIVATE_KEY env variable is required to sign non-direct interaction tx"
+      );
+    }
+
+    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const signed_tx = await signer.signTransaction(new_raw_tx);
+
+    return { gas_estimate_tx: signed_tx, raw_tx: new_raw_tx };
+  } else {
+    const new_raw_tx = {
+      to: ethflow_address,
+      data: "0x7bc41b96" + tx.data.substring(10),
+      value: ethers.utils.parseUnits("0", "ether"),
+    };
+
+    return { gas_estimate_tx: new_raw_tx, raw_tx: new_raw_tx };
+  }
 }
 
 main();
